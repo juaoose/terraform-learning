192999dc98a220c7479e9602e9c03048e15fda3a
Juan José <jjrg1994@gmail.com>
2019-10-17 23:23:48 -0500

    add variables and outputs

--- main.tf
+++ main.tf
@@ -3,6 +3,12 @@ provider "aws" {
   profile = "lean-jj"
 }
 
+variable "server_port" {
+  description = "Port that the server will serve requests on"
+  type        = number
+  default     = 8080
+}
+
 resource "aws_instance" "example" {
   ami                    = "ami-04b9e92b5572fa0d1"
   instance_type          = "t2.micro"
@@ -11,7 +17,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello Cloud Gurus" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   tags = {
@@ -23,10 +29,14 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "Public IP assigned to the instance" 
+}

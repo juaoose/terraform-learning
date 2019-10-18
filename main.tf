81503829a24441c16d65d624f5deda2eadc44e95
Juan José <jjrg1994@gmail.com>
2019-10-17 23:00:15 -0500

    add simple web server

--- main.tf
+++ main.tf
@@ -1,13 +1,32 @@
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
   profile = "lean-jj"
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-0b69ea66ff7391e80"
-  instance_type = "t2.micro"
+  ami                    = "ami-04b9e92b5572fa0d1"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello Cloud Gurus" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags = {
     Name = "terraform-example"
   }
 }
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
